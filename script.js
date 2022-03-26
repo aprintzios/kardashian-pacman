3fd557957a1863776fc12ce698efdf95db9b69da
Alexandra Printzios <aprintzios@Alexandras-MacBook-Air.local>
2022-03-26 18:33:37 -0400

    node, player, ghost, gem, wall classes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 class Node{
     constructor(val){
         this.value = val;
+        this.type = "node";
         this.domEl = this.getDOMEl();
         this.contains = [];
     }
@@ -12,7 +13,6 @@ class Node{
         } else{
             this.domEl.style.backgroundImage = "url(./img/" + update + ".png)";
             this.domEl.style.backgroundSize = "contain";
-
         }
     }
     getBG(){
@@ -34,39 +34,185 @@ class Node{
     }
 }
 class Player{
-    constructor(){
-        
+    constructor(node){
+        this.node = node;
+        this.type = "player";
+    }
+    drawPlayer(){
+        if (kLeft){
+            this.node.updateBG('kLeft', true);
+        } else {
+            this.node.updateBG('kRight', true);
+        }
     }
 }
 class Ghost{
-    constructor(){
-        
+    constructor(node){
+        this.node = node;
+        this.type = "player";
+    }
+    drawGhost(ghostName){
+        this.node.updateBG(ghostName, true);
     }
 }
 class Gem{
-    constructor(){
-        
+    constructor(node){
+        this.node = node;
+        this.type = "gem";
+    }
+    drawGem(){
+        this.node.updateBG('gem', true);
     }
 }
 class Wall{
     constructor(node){
         this.node = node;
+        this.type = "wall";
     }
     drawWall(){
-        
+        this.node.updateBG('wall', true);
     }
 }
 
 //vars/constants
-let NUM_ROWS = 15; 
-let NUM_COLS = 15;
+let NUM_ROWS = 11; 
+let NUM_COLS = 9;
 let boardSetup = false;
 let nodes = [];
-let map = [
-    ['1','-','-', '2'],
-    ['-','-','-', '-'],
-    ['4','-','-', '3']
-    ];
+let kim;
+let kanye;
+let kris;
+let kLeft = false;
+let walls = [
+    [1,1],
+    [1,3],
+    [1,4],
+    [1,5],
+    [2,4],
+    [1,7],
+    [3,1],
+    [3,2],
+    [3,6],
+    [3,7],
+    [5,1],
+    [5,3],
+    [5,4],
+    [5,5],
+    [4,4],
+    [6,4],
+    [5,7],
+    [7,1],
+    [7,2],
+    [7,6],
+    [7,7],
+    [9,1],
+    [9,3],
+    [9,4],
+    [9,5],
+    [8,4],
+    [9,7]
+];
+let gems = [
+    //[0,0],
+    [0,1],
+    [0,2],
+    [0,3],
+    [0,4],
+    [0,5],
+    [0,6],
+    [0,7],
+    [0,8],
+    [1,0],
+    //[1,1],
+    [1,2],
+    //[1,3],
+    //[1,4],
+    //[1,5],
+    [1,6],
+    //[1,7],
+    [1,8],
+    [2,0],
+    [2,1],
+    [2,2],
+    [2,3],
+    //[2,4],
+    [2,5],
+    [2,6],
+    [2,7],
+    [2,8],
+    [3,0],
+    //[3,1],
+    //[3,2],
+    [3,3],
+    [3,4],
+    [3,5],
+    //[3,6],
+    //[3,7],
+    [3,8],
+    [4,0],
+    [4,1],
+    [4,2],
+    [4,3],
+    //[4,4],
+    [4,5],
+    [4,6],
+    [4,7],
+    [4,8],
+    [5,0],
+    //[5,1],
+    [5,2],
+    //[5,3],
+    //[5,4],
+    //[5,5],
+    [5,6],
+    //[5,7],
+    [5,8],
+    [6,0],
+    [6,1],
+    [6,2],
+    [6,3],
+    //[6,4],
+    [6,5],
+    [6,6],
+    [6,7],
+    [6,8],
+    [7,0],
+    //[7,1],
+    //[7,2],
+    [7,3],
+    [7,4],
+    [7,5],
+    //[7,6],
+    //[7,7],
+    [7,8],
+    [8,0],
+    [8,1],
+    [8,2],
+    [8,3],
+    //[8,4],
+    [8,5],
+    [8,6],
+    [8,7],
+    [8,8],
+    [9,0],
+    //[9,1],
+    [9,2],
+    //[9,3],
+    //[9,4],
+    //[9,5],
+    [9,6],
+    //[9,7],
+    [9,8],
+    [10,0],
+    [10,1],
+    [10,2],
+    [10,3],
+    [10,4],
+    [10,5],
+    [10,6],
+    [10,7],
+    //[10,8]
+];
 
 //cache elements
 let btn = document.getElementById("restart");
@@ -83,6 +229,7 @@ btn.addEventListener("click", restart);
 
 //functions
 function init(){
+
     //arrow key event lsitener
     document.addEventListener("keydown", arrowHandler);
     //initialize DOM
@@ -91,7 +238,6 @@ function init(){
             let row = document.createElement("div");
             row.classList.add("row");
             board.appendChild(row);
-            //<div id="sqr00" class="square"></div>
             for (let j=0; j<NUM_COLS; j++){
                 let sqr = document.createElement("div");
                 sqr.classList.add("square");
@@ -114,19 +260,38 @@ function init(){
     boardSetup = true;
 
     //initialize node array
-    let sqr = 0;
     for (let i=0; i<NUM_ROWS; i++){
         nodes[i] = [];
         for (let j=0; j<NUM_COLS; j++){
-            let newNode = new Node(sqr);
+            let newNode = new Node(coordToNode(i,j));
             nodes[i].push(newNode);
-            sqr++;
         }
     }
+    
 
-    let wallX = 4;
-    let wallY = 4;
-    let newWall = new Wall();
+    //initialize walls
+    walls.forEach((element)=>{
+        let newWall = new Wall(nodes[element[0]][element[1]]);
+        nodes[element[0]][element[1]].contains.push(newWall);
+        newWall.drawWall();
+    })
+
+    //initialize gems
+    gems.forEach((element)=>{
+        let newGem = new Gem(nodes[element[0]][element[1]]);
+        nodes[element[0]][element[1]].contains.push(newGem);
+        newGem.drawGem();
+    })
+
+    //initialize player(kim)
+    kim = new Player(nodes[0][0]);
+    nodes[0][0].contains.push(kim);
+    kim.drawPlayer();
+
+    //initialize ghosts
+    kanye = new Ghost(nodes[10][8]);
+    nodes[0][0].contains.push(kanye);
+    kanye.drawGhost('kanye');
 
 }
 
@@ -148,25 +313,116 @@ function gameOver(){
 function render(){
 
 }
-function drawMap(){
-
-}
-function drawPlayer(){
-
-}
-function drawGhosts(){
-
-}
-function drawGems(){
-
-}
-
 //helper functions
 function nodeToCoord(node){
-    let x = Math.floor(node/NUM_ROWS);
-    let y = node%NUM_ROWS;
+    let x = Math.floor(node/NUM_COLS);
+    let y = node%NUM_COLS;
     return [x,y];
 }
 function coordToNode(x,y){
-    return ((x*NUM_ROWS)+(y));
+    return ((x*NUM_COLS)+(y));
 }
+
+
+//full array
+let allSquares = [
+[0,0],
+[0,1],
+[0,2],
+[0,3],
+[0,4],
+[0,5],
+[0,6],
+[0,7],
+[0,8],
+[1,0],
+[1,1],
+[1,2],
+[1,3],
+[1,4],
+[1,5],
+[1,6],
+[1,7],
+[1,8],
+[2,0],
+[2,1],
+[2,2],
+[2,3],
+[2,4],
+[2,5],
+[2,6],
+[2,7],
+[2,8],
+[3,0],
+[3,1],
+[3,2],
+[3,3],
+[3,4],
+[3,5],
+[3,6],
+[3,7],
+[3,8],
+[4,0],
+[4,1],
+[4,2],
+[4,3],
+[4,4],
+[4,5],
+[4,6],
+[4,7],
+[4,8],
+[5,0],
+[5,1],
+[5,2],
+[5,3],
+[5,4],
+[5,5],
+[5,6],
+[5,7],
+[5,8],
+[6,0],
+[6,1],
+[6,2],
+[6,3],
+[6,4],
+[6,5],
+[6,6],
+[6,7],
+[6,8],
+[7,0],
+[7,1],
+[7,2],
+[7,3],
+[7,4],
+[7,5],
+[7,6],
+[7,7],
+[7,8],
+[8,0],
+[8,1],
+[8,2],
+[8,3],
+[8,4],
+[8,5],
+[8,6],
+[8,7],
+[8,8],
+[9,0],
+[9,1],
+[9,2],
+[9,3],
+[9,4],
+[9,5],
+[9,6],
+[9,7],
+[9,8],
+[10,0],
+[10,1],
+[10,2],
+[10,3],
+[10,4],
+[10,5],
+[10,6],
+[10,7],
+[10,8]
+];
