128637462fd9acec8c238e227b6aa05501077ffa
Alexandra Printzios <aprintzios@Alexandras-MacBook-Air.local>
2022-03-28 16:24:08 -0400

    adding audio

--- script.js
+++ script.js
@@ -94,8 +94,6 @@ class Wall{
 }
 
 //vars/constants
-// let NUM_ROWS = 11; 
-// let NUM_COLS = 9;
 let NUM_ROWS = 9; 
 let NUM_COLS = 20;
 let boardSetup = false;
@@ -173,166 +171,166 @@ let gemsMap = [
     [0,17],
     [0,18],
     [0,19],
-    [1,0],
-    //[1,1],
-    [1,2],
-    //[1,3], 
-    //[1,4], 
-    //[1,5], 
-    [1,6],
-    //[1,7], 
-    [1,8],
-    [1,9],
-    [1,10],
-    [1,11],
-    //[1,12],
-    [1,13],
-    //[1,14],
-    //[1,15],
-    //[1,16],
-    [1,17],
-    //[1,18],
-    [1,19],
-    [2,0],
-    [2,1],
-    [2,2],
-    [2,3],
-    //[2,4],
-    [2,5],
-    [2,6],
-    [2,7],
-    [2,8],
-    [2,9],
-    [2,10],
-    [2,11],
-    [2,12],
-    [2,13],
-    [2,14],
-    //[2,15],
-    [2,16],
-    [2,17],
-    [2,18],
-    [2,19],
-    [3,0],
-    //[3,1],
-    //[3,2],
-    [3,3],
-    [3,4],
-    [3,5],
-    //[3,6],
-    //[3,7],
-    [3,8],
-    [3,9],
-    [3,10],
-    [3,11],
-    //[3,12],
-    //[3,13],
-    [3,14],
-    [3,15],
-    [3,16],
-    //[3,17],
-    //[3,18],
-    [3,19],
-    [4,0],
-    [4,1],
-    [4,2],
-    [4,3],
-    //[4,4],
-    [4,5],
-    [4,6],
-    [4,7],
-    [4,8],
-    [4,9],
-    [4,10],
-    [4,11],
-    [4,12],
-    [4,13],
-    [4,14],
-    //[4,15],
-    [4,16],
-    [4,17],
-    [4,18],
-    [4,19],
-    [5,0],
-    //[5,1],
-    [5,2],
-    //[5,3],
-    //[5,4],
-    //[5,5],
-    [5,6],
-    //[5,7],
-    [5,8],
-    [5,9],
-    [5,10],
-    [5,11],
-    //[5,12],
-    [5,13],
-    //[5,14],
-    //[5,15],
-    //[5,16],
-    [5,17],
-    //[5,18],
-    [5,19],
-    [6,0],
-    [6,1],
-    [6,2],
-    [6,3],
-    //[6,4],
-    [6,5],
-    [6,6],
-    [6,7],
-    [6,8],
-    [6,9],
-    [6,10],
-    [6,11],
-    [6,12],
-    [6,13],
-    [6,14],
-    //[6,15],
-    [6,16],
-    [6,17],
-    [6,18],
-    [6,19],
-    [7,0],
-    //[7,1],
-    //[7,2],
-    [7,3],
-    [7,4],
-    [7,5],
-    //[7,6],
-    //[7,7],
-    [7,8],
-    [7,9],
-    [7,10],
-    [7,11],
-    //[7,12],
-    //[7,13],
-    [7,14],
-    [7,15],
-    [7,16],
-    //[7,17],
-    //[7,18],
-    [7,19],
-    [8,0], //ghost position
-    [8,1],
-    [8,2],
-    [8,3],
-    //[8,4],
-    [8,5],
-    [8,6],
-    [8,7],
-    [8,8], //ghost position
-    [8,9],
-    [8,10],
-    [8,11],
-    [8,12],
-    [8,13],
-    [8,14],
-    //[8,15],
-    [8,16],
-    [8,17],
-    [8,18],
-    [8,19]
+    // [1,0],
+    // //[1,1],
+    // [1,2],
+    // //[1,3], 
+    // //[1,4], 
+    // //[1,5], 
+    // [1,6],
+    // //[1,7], 
+    // [1,8],
+    // [1,9],
+    // [1,10],
+    // [1,11],
+    // //[1,12],
+    // [1,13],
+    // //[1,14],
+    // //[1,15],
+    // //[1,16],
+    // [1,17],
+    // //[1,18],
+    // [1,19],
+    // [2,0],
+    // [2,1],
+    // [2,2],
+    // [2,3],
+    // //[2,4],
+    // [2,5],
+    // [2,6],
+    // [2,7],
+    // [2,8],
+    // [2,9],
+    // [2,10],
+    // [2,11],
+    // [2,12],
+    // [2,13],
+    // [2,14],
+    // //[2,15],
+    // [2,16],
+    // [2,17],
+    // [2,18],
+    // [2,19],
+    // [3,0],
+    // //[3,1],
+    // //[3,2],
+    // [3,3],
+    // [3,4],
+    // [3,5],
+    // //[3,6],
+    // //[3,7],
+    // [3,8],
+    // [3,9],
+    // [3,10],
+    // [3,11],
+    // //[3,12],
+    // //[3,13],
+    // [3,14],
+    // [3,15],
+    // [3,16],
+    // //[3,17],
+    // //[3,18],
+    // [3,19],
+    // [4,0],
+    // [4,1],
+    // [4,2],
+    // [4,3],
+    // //[4,4],
+    // [4,5],
+    // [4,6],
+    // [4,7],
+    // [4,8],
+    // [4,9],
+    // [4,10],
+    // [4,11],
+    // [4,12],
+    // [4,13],
+    // [4,14],
+    // //[4,15],
+    // [4,16],
+    // [4,17],
+    // [4,18],
+    // [4,19],
+    // [5,0],
+    // //[5,1],
+    // [5,2],
+    // //[5,3],
+    // //[5,4],
+    // //[5,5],
+    // [5,6],
+    // //[5,7],
+    // [5,8],
+    // [5,9],
+    // [5,10],
+    // [5,11],
+    // //[5,12],
+    // [5,13],
+    // //[5,14],
+    // //[5,15],
+    // //[5,16],
+    // [5,17],
+    // //[5,18],
+    // [5,19],
+    // [6,0],
+    // [6,1],
+    // [6,2],
+    // [6,3],
+    // //[6,4],
+    // [6,5],
+    // [6,6],
+    // [6,7],
+    // [6,8],
+    // [6,9],
+    // [6,10],
+    // [6,11],
+    // [6,12],
+    // [6,13],
+    // [6,14],
+    // //[6,15],
+    // [6,16],
+    // [6,17],
+    // [6,18],
+    // [6,19],
+    // [7,0],
+    // //[7,1],
+    // //[7,2],
+    // [7,3],
+    // [7,4],
+    // [7,5],
+    // //[7,6],
+    // //[7,7],
+    // [7,8],
+    // [7,9],
+    // [7,10],
+    // [7,11],
+    // //[7,12],
+    // //[7,13],
+    // [7,14],
+    // [7,15],
+    // [7,16],
+    // //[7,17],
+    // //[7,18],
+    // [7,19],
+    // [8,0], //ghost position
+    // [8,1],
+    // [8,2],
+    // [8,3],
+    // //[8,4],
+    // [8,5],
+    // [8,6],
+    // [8,7],
+    // [8,8], //ghost position
+    // [8,9],
+    // [8,10],
+    // [8,11],
+    // [8,12],
+    // [8,13],
+    // [8,14],
+    // //[8,15],
+    // [8,16],
+    // [8,17],
+    // [8,18],
+    // [8,19]
 
 ];
 let emptyMap = [
@@ -527,14 +525,16 @@ let level = 1;
 
 //cache elements
 let btn = document.getElementById("restart");
-let levelEl = document.getElementById("level");
 let board = document.querySelector(".board");
 let resultEl = document.getElementById("result");
 let overlayWinEl = document.getElementById("overlayWin");
 let overlayLoseEl = document.getElementById("overlayLose");
 let nextLvlBtn = document.getElementById("nextLvl");
 let loseRestartBtn = document.getElementById("loseRestart");
-let lvlEl = document.getElementById("level");
+let lvlImg = document.getElementById("levelImg");
+let blingAudio = document.getElementById("bling");
+let kimLoseAudio = document.getElementById("kimLoseAudio");
+let kimWinAudio = document.getElementById("kimWinAudio");
 
 //initialize
 init();
@@ -548,7 +548,7 @@ function init(){
     win = false;
     overlayWinEl.style.display = "none";
     overlayLoseEl.style.display = "none";
-    lvlEl.innerText = level;
+    lvlImg.src="./img/level" +level+ ".png"
     //arrow key event lsitener
     document.addEventListener("keydown", playerMove);
     //initialize DOM
@@ -635,7 +635,7 @@ function init(){
     kim.drawPlayer();
     boardSetup = true;
     } else{
-        //empty map
+        //clear map
         emptyMap.forEach((element)=>{
             clearNode(nodes[element[0]][element[1]]);
         });
@@ -658,14 +658,9 @@ function init(){
         newGem.drawGem();
     })
 
-
-    //clear all nodes with no player, gem, or ghost
-    emptyMap.forEach((element)=>{
-        let elNode = coordToNode(element[0], element[1]);
-    });
-
     //initialize ghostMove interval
     intervId = setInterval(ghostMove, 1000/level);
+
     render();
 }
 
@@ -746,8 +741,10 @@ function ghostMove(){
             clearNode(ghost.node);
         }
         //get next move
-        let nextMove = mP.pop(); //throw away current location
-        nextMove = mP.pop(); //get next move
+        let nextMove = mP.pop();
+        // nextMove = mP.pop(); //get next move
+        // mP.pop(); //throw away current location
+        // let nextMove = mP.pop(); //get next move
         if (!nextMoves.includes(nextMove)){
             nextMoves.push(nextMove);
             let nextCoords = nodeToCoord(nextMove);
@@ -763,7 +760,7 @@ function ghostMove(){
 function collisionDetectionWall(node){
     //check for node/wall collision
     let collision = false;
-    walls.forEach((element, i)=>{
+    walls.forEach((element)=>{
         if (element.node.value === node.value){
             collision = true;
         }
@@ -774,9 +771,11 @@ function collisionDetectionKimGem(node){
     //check for kim/gem collision
     gems.forEach((gem, i)=>{
         if (gem.node.value === node.value){
+            blingAudio.src = "";
+            blingAudio.src = "./audio/bling.wav";
+            blingAudio.play();
             score++;
             gems.splice(i,1);
-            //pointsEl.innerText = score;
         }
     });
 
@@ -805,14 +804,16 @@ function collisionDetectionGhost(node){
 
 function gameOver(){
     clearInterval(intervId);
+    document.removeEventListener("keydown", playerMove);
     if (win){
-        document.removeEventListener("keydown", playerMove);
         overlayWinEl.style.display = "block";
         level++;
         nextLvlBtn.addEventListener("click", goToNextLvl);
+        kimWinAudio.play();
     } else{
         overlayLoseEl.style.display = "block";
         loseRestartBtn.addEventListener("click", restart);
+        kimLoseAudio.play();
     }   
 }
 function restart(){
@@ -820,7 +821,6 @@ function restart(){
     clearInterval(intervId);
     init();
 }
-
 //rendering function
 function render(){
     //render gems
@@ -853,7 +853,8 @@ function clearNode(node){
 }
 function findMP(source, end){
     let result = dijkstra(NUM_ROWS*NUM_COLS,adj,source,end);
-    let minPath = [end];
+    //let minPath = [end];
+    let minPath = [];
     while (result[1] !== source){
         minPath.unshift(result[1]);
         result = dijkstra(NUM_ROWS*NUM_COLS,adj,source,result[1]);
